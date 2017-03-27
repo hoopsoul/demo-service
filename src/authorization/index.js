e04187f3b88a8c3e69d2220a8036f5276bd95c31
Qiang <fuqiang@outlook.com>
2017-03-28 00:10:04 +0800

    remove useless codes

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,37 +1,14 @@
 import { errors } from 'demo-model'
-import Hashids from 'hashids'
 import * as exemptions from './exemptions'
-
-let hash
 
 function authorize(req) {
   if (!exemptions.has(req.url)) {
-    // const { id, token } = req.merchant
-    // if (!id || !token) {
     return new errors.UnauthorizedError()
-    // }
   }
 }
 
-function parse(stub) {
-  if (!stub || typeof stub !== 'string') return {}
-  const [token, idHash, shopHash] = stub.split('.')
-  const [id] = hash.decode(idHash)
-  const shop = shopHash ? { id: hash.decode(shopHash)[0] } : {}
-  return { token, id, shop }
-}
-
-export const sign = (token, merchant, shop) => {
-  const hash1 = hash.encode(merchant.id)
-  const _shop = shop || merchant.shop || {}
-  const hash2 = _shop.id ? hash.encode(_shop.id) : ''
-  return `${token}.${hash1}.${hash2}`
-}
-
 export default (config) => {
-  hash = new Hashids(config.secret.hash, 8)
   return (req, res, next) => {
-    // req.merchant = parse(req.signedCookies.YTAUTOKEN)
     next(authorize(req))
   }
 }
